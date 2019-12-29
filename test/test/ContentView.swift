3d3d8c1ab8ca95f290ec72d0265528c2ebe5b40c
jun <aBetterDayAndLife@gmail.com>
2019-12-29 17:39:36 +0900

    테스트 프로젝트 생성

---
+++ test/test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Sang jun Park on 2019/12/29.
+//  Copyright © 2019 Sang jun Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
